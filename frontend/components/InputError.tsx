2d353c1d40b05f11daa6d6bb35612ff83517a279
alazaradane <alazargithub369@gmail.com>
2024-12-13 12:26:51 +0300

    Next.js frontend setup

---
+++ frontend/components/InputError.tsx
@@ -0,0 +1,23 @@
+
+interface InputErrorProps {
+    className: string
+    messages: any | undefined | never[]
+}
+
+const InputError = ({ messages = [], className = '' }) => (
+    <>
+        {messages.length > 0 && (
+            <>
+                {messages.map((message, index) => (
+                    <p
+                        className={`${className} text-sm text-red-600`}
+                        key={index}>
+                        {message}
+                    </p>
+                ))}
+            </>
+        )}
+    </>
+)
+
+export default InputError
